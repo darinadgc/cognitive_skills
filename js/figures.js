9a5248f7362ebc2fd61b63d18b312567b15ee808
Дарина <69759782+darinadgc@users.noreply.github.com>
2025-04-02 14:44:24 +0300

    Update figures.js

--- js/figures.js
+++ js/figures.js
@@ -123,8 +123,8 @@ function checkAnswer(selectedIndex) {
            // }
         //});
 header.classList.add("low-opacity"); 
-    main.style.display("none"); 
-footer.style.display("none"); 
+    main.style.display"none"; 
+footer.style.display"none"; 
 //("header, main, footer").forEach(el =>{if (!el.matches("#game-container") && !el.matches("#timer")) {
         //el.classList.add("low-opacity");
     //}
@@ -169,8 +169,8 @@ window.calculateScore = function(timeTaken) {
     figureTaskEl.innerHTML = "";
     sendResultsBtn.style.display = "block";
 header.classList.remove("low-opacity"); 
-    main.display.style("block"); 
-footer.display.style("block"); 
+    main.display.style"block"; 
+footer.display.style"block"; 
     // ✅ Збереження finalScore в глобальну змінну
     // window.finalScore = calculateScore(window.timeTaken);
    //  console.log("🎯 Обчислений бал:", window.finalScore);
